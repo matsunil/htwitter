fd65dc95f1b3b011361a0d327883b35fa07391d8
sunilmathew <mat_sunil@hotmail.com>
2015-03-07 14:06:33 -0500

    made tweets create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,15 @@ class TweetsController < ApplicationController
 		@tweet = Tweet.new
 	end
 
+	#action create
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+		render 'new'
+	end
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
+
 end
