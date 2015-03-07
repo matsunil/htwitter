4b48966ed1e779db4bd3f8a1ce3ef3d9e1732b97
sunilmathew <mat_sunil@hotmail.com>
2015-03-07 17:48:49 -0500

    user authentication

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
 
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
 	end
@@ -7,6 +9,7 @@ class TweetsController < ApplicationController
 	#action create
 	def create
 		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
 		if @tweet.save
 			flash[:success] = "You have created a tweet"
 			redirect_to new_tweet_path
